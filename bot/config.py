e39e1ad4b4745e8df4937a69efb08d6cc6759295
Akshadgod <157225495+Akshadgod@users.noreply.github.com>
2024-09-17 21:21:18 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,9 +5,9 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = config("27981265", cast=int)
+    API_HASH = config("e2e52a1550b8dea246b64617a768eee2")
+    BOT_TOKEN = config("6835393349:AAFMfsfQ9ctPb4Cml1HIDMbAj73pEJfOxX4")
     DEV = 1664850827
     OWNER = config("OWNER")
     ffmpegcode = ["-map 0 -c:v libx264 -pix_fmt yuv420p -crf 30 -preset veryfast -s 856x480 -c:a aac -b:a 50k -c:s copy"]
