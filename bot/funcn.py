a7936e80e3f95d209ad00f93e6f53f931620a391
Incredaimaso <159280005+Incredaimaso@users.noreply.github.com>
2025-01-05 19:58:30 +0530

    ss

--- bot/funcn.py
+++ bot/funcn.py
@@ -116,7 +116,7 @@ async def test(event):
 
 
 async def sysinfo(e):
-    if str(e.sender_id) not in OWNER and event.sender_id !=DEV:
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
         return
     total, used, free, disk= disk_usage('/')
     total = hbs(total)
@@ -183,7 +183,7 @@ async def skip(e):
 
 
 async def renew(e):
-    if str(e.sender_id) not in OWNER and event.sender_id !=DEV:
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
         return
     await e.reply("**Cleared Queued, Working Files and Cached Downloads!**")
     WORKING.clear()
@@ -200,7 +200,7 @@ async def renew(e):
 
 
 async def coding(e):
-    if str(e.sender_id) not in OWNER and event.sender_id !=DEV:
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
         return
     ffmpeg = e.text.split(" ", maxsplit=1)[1]
     ffmpegcode.clear()
@@ -210,26 +210,26 @@ async def coding(e):
 
 
 async def getlogs(e):
-    if str(e.sender_id) not in OWNER and event.sender_id !=DEV:
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
         return
     await e.client.send_file(e.chat_id, file=LOG_FILE_NAME, force_document=True)
 
 
 async def getthumb(e):
-    if str(e.sender_id) not in OWNER and event.sender_id !=DEV:
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
         return
     await e.client.send_file(e.chat_id, file="/bot/thumb.jpg", force_document=False, caption="**Your Current Thumbnail.**")
 
 
 async def getcode(e):
-    if str(e.sender_id) not in OWNER and event.sender_id !=DEV:
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
         return
     await e.reply(f"**Your Current FFMPEG Code is**\n\n`{ffmpegcode[0]}`")
     return
 
 
 async def clearqueue(e):
-    if str(e.sender_id) not in OWNER and event.sender_id !=DEV:
+    if str(e.sender_id) not in OWNER and e.sender_id !=DEV:
         return
     await e.reply("**Cleared Queued Files!**")
     QUEUE.clear()
